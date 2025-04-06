d2ff29ef2393a221fa10150e8072adf8dc443d91
Trevor Wooten <twootproductions16@gmail.com>
2025-04-06 18:01:54 -0400

    Slight modifications to word label

--- py_note.py
+++ py_note.py
@@ -3,7 +3,7 @@ from tkinter import filedialog #for saving
 from tkinter import colorchooser #changing background
 
 def open_window():
-    
+
     #Creating window
     window = tk.Tk()
 
@@ -14,20 +14,20 @@ def open_window():
 
     #title
     window.title("Harvest Note By Trevor Wooten")
-    
+
     #making toolbar frame
     toolbar_frame = tk.Frame(window, height= 25, bg="black")
     toolbar_frame.pack(side="top", fill="x")
 
     # title label
     title_label = tk.Label(window, text="Harvest Note", font=("Arial", 20))
-    
+
     # Centering title label
     title_label.pack(expand= True, pady=10)
 
     #Making text area variable
     text_box = text_area(window)
-    
+
     #Showing toolbar
     toolbar_buttons(window, toolbar_frame, text_box)
 
@@ -35,47 +35,47 @@ def open_window():
     word_counter(window, text_box)
 
 
-    
+
     #Size of window width x height
     window.geometry("700x650")
-    
+
     #Size limits
     window.minsize(200, 650)
     window.maxsize(1200, 650)
-    
+
     #Window color
     window.configure(background="orange")
 
-  
-    
+
+
 
     #Running application
     window.mainloop()
-    
-    
-    
+
+
+
 def text_area(window):
 
     #text area frame
     text_frame = tk.Frame(window, width=1250, height=450)
-    
+
     #Fitting frame the length of screen
     text_frame.pack(expand= True, fill= "x", padx= 5, pady=(0, 20))
-    
-    
+
+
     #making text box
     text_area = tk.Text(text_frame, font=("Arial", 12), wrap="word")
     text_area.pack(expand= True, fill="both")
 
     text_area.config(height=29)
-    
+
     #returning this for the buttons
     return text_area
-    
-    
-    
+
+
+
 def toolbar_buttons(window, toolbar_frame, text_box):
-    
+
     #Buttons and adding the methods
     save_button = tk.Button(toolbar_frame, text="Save", bg="white", command=lambda: save_file(text_box))
     open_button = tk.Button(toolbar_frame, text="Open", bg="white", command=lambda: open_file(text_box))
@@ -83,17 +83,17 @@ def toolbar_buttons(window, toolbar_frame, text_box):
 
     #Switch button
     change_count_button = tk.Button(toolbar_frame, text="Word/Character Count", bg="white")
-  
-    
+
+
     #Positioning
     save_button.pack(side="left", padx= 10)
     open_button.pack(side="left", padx= 10)
     style_button.pack(side="left", padx= 10)
     change_count_button.pack(side="right", padx= 10)
-    
-    
+
+
 def save_file(text_box):
-    
+
     #Askinf user where to put file
     file_path = filedialog.asksaveasfilename( defaultextension= ".txt", filetypes=[("*.txt", "*.txt")], initialfile="Name your document here!")
 
@@ -102,10 +102,10 @@ def save_file(text_box):
         with open(file_path, "w", encoding="utf-8") as file:
             content = text_box.get("1.0", "end-1c")
             file.write(content)
-    
+
 
 def open_file(text_box):
-    
+
     #Asking user what file to open
     file_path = filedialog.askopenfilename(defaultextension= ".txt", filetypes=[("*.txt", "*.txt")])
 
@@ -113,10 +113,10 @@ def open_file(text_box):
     if file_path:
         with open(file_path, "r", encoding="utf-8") as file:
             #Reading file
-            content = file.read() 
+            content = file.read()
             text_box.delete("1.0", "end")
             text_box.insert("1.0", content)
-    
+
 
 #Change background
 def change_bg(window):
@@ -131,8 +131,13 @@ def change_bg(window):
 
 def word_counter(window, text_box):
 
-    word_label = tk.Label(window, text="Words: 0", fg="white", bg="black", font=("Arial", 12))
-    word_label.pack(side="bottom", anchor="center", padx= 10)
+    #Showing words
+    word_label = tk.Label(window, text="Words: 0", fg="white", bg="black", font=("Arial", 12), padx= 1)
+    word_label.pack(side="bottom", anchor="w", ipadx= 10)
+
+    #showing characters
+    # character_label = tk.Label(window, text="Characters: 0", fg="white", bg="black", font=("Arial", 12))
+    # character_label.pack(anchor="center")
 
     def update_counter(event=None):
 
@@ -147,42 +152,40 @@ def word_counter(window, text_box):
         #Binding it to the word label
         word_label.config(text=f"Words: {words}")
 
-    #Binding to key release 
+        # character_label.config(text=f"Characters: {characters}")
+
+
+    #Binding to key release
     text_box.bind("<KeyRelease>", update_counter)
 
     return word_label
 
 
 
+def switch_count_display(window, text_box):
+    if toggle_switch.get() == "words":
+        toggle_switch.set("characters")
+    else:
+        toggle_switch.set("words")
+
+    word_counter(window, text_box)
 
 
 
 
-# def switch_count_display(text_box, word_label):
-#     if toggle_switch.get() == "words":
-#         toggle_switch.set("characters")
-#     else:
-#         toggle_switch.set("words")
-#         #Updating label
-#         update_counter(text_box, word_label)
 
-
-
-
-    
-    
 #Calling functino
 def main():
-    
+
     #opening window
     open_window()
-    
 
-  
-    
-    
+
+
+
+
 if __name__ == "__main__":
     main()
-    
+
 
 
